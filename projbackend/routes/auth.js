9a57e30f3506f26572ecd5eadc907de0cbda373c
Vaibhav Bhapkar <vaibhavbhapkar62@gmail.com>
2021-07-15 18:19:08 +0530

    worked on validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator');
 const {signout, signin, signup} = require("../controllers/auth")
 
-router.post("/signup", signup);
+router.post("/signup",[
+    check("name","name should be at least 3 char").isLength({ min: 5 }),
+    check("email","email is required").isEmail(),
+    check("password","password should be at least 3 char").isLength({ min: 5 }),
+], signup);
 router.get("/signout", signout);
 router.get("/signin", signin);
 
