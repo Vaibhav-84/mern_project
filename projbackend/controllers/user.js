f54ad4a48e5c512ce1f0c2e623d2d8c5f71417b7
Vaibhav Bhapkar <vaibhavbhapkar62@gmail.com>
2021-08-18 22:03:56 +0530

    Update user data

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -13,10 +13,25 @@ exports.getUserById = (req, res, next, id)=>{
 }
 
 exports.getUser = (req, res)=>{
-    //TODO: get back here for password
     req.profile.salt = undefined;
     req.profile.encry_password = undefined;
-    req.profile.createdAt = undefined;
-    req.profile.updatedAt = undefined;
     return res.json(req.profile);
 };
+
+exports.updateUser = (req, res)=>{
+    User.findByIdAndUpdate(
+        {_id : req.profile._id},
+        {$set: req.body},
+        {new: true, useFindAndModify: false},
+        (err, user)=>{
+            if(err){
+                return res.status(404).json({
+                    error: "not allowed"
+                })
+            }
+            user.salt = undefined;
+            user.encry_password = undefined;
+            res.json(user);
+        }
+    )
+}
